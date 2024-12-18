e4f5206d2966f9eb1f97e80ba094fe0edf32bd12
adampermana <adampermana111@gmail.com>
2024-12-18 13:55:53 +0700

    feat: - add jar - add Configuration build.gradle - configuration AndroidManifest.xml - Configuration pubspec.yaml

---
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,15 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_smart_pin_pad_cards/flutter_smart_pin_pad_cards_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) flutter_smart_pin_pad_cards_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterSmartPinPadCardsPlugin");
+  flutter_smart_pin_pad_cards_plugin_register_with_registrar(flutter_smart_pin_pad_cards_registrar);
+}
